363579e0c1cd4c3d7befb0701461ddbcce6b0319
thaontl <thaontl@shinway.net>
2019-03-07 15:49:21 +0700

    22812

--- resources/assets/js/service/suppliers-service.js
+++ resources/assets/js/service/suppliers-service.js
@@ -14,13 +14,6 @@ suppliers_service = {
             return error;
         });
     },
-    gotoCreate: function (data) {
-        return axios.post('/suppliers/create', data).then(function (response) {
-            return response.data;
-        }).catch(function (error) {
-            return error;
-        });
-    },
     deleteSupplier: function (id) {
         return axios.get('/suppliers/delete/' + id).then(function (response) {
             return response.data;
