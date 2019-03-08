ec92afee50dc4eea840456c69c75881f6a26d198
thaontl <thaontl@shinway.net>
2019-03-08 14:33:52 +0700

    22841

--- app/Models/MSupplier.php
+++ app/Models/MSupplier.php
@@ -62,30 +62,6 @@ class MSupplier extends Model
 
     ];
 
-    public function getSuppliers($where = array()){
-        $suppliers = new MSupplier();
-        $suppliers = $suppliers->select(DB::raw('mst_suppliers.*'))
-                                ->addselect('mst_general_purposes.date_nm');
-        $suppliers = $suppliers->leftjoin('mst_general_purposes', function ($join) {
-            $join->on('mst_general_purposes.date_id', '=', 'mst_suppliers.prefectures_cd')
-                    ->where('mst_general_purposes.data_kb', config('params.data_kb')['prefecture']);
-        });
-
-        // 検索条件
-        if (isset($where['suppliers_cd']) && $where['suppliers_cd'] != '')
-            $suppliers = $suppliers->where('mst_suppliers_cd', "LIKE", "%{$where['suppliers_cd']}%");
-        if (isset($where['supplier_nm']) && $where['supplier_nm'] != '')
-            $suppliers = $suppliers->where('supplier_nm', "LIKE", "%{$where['supplier_nm']}%");
-        if (isset($where['reference_date']) && $where['reference_date'] != '') {
-            $suppliers = $suppliers->where('adhibition_start_dt', "<=", $where['reference_date']);
-            $suppliers = $suppliers->where('adhibition_end_dt', ">=", $where['reference_date']);
-        }
-
-        $suppliers->orderBy('mst_suppliers_cd', 'adhibition_start_dt');
-
-        return $suppliers->get();
-    }
-
     public function getHistoryNearest($supplier_cd, $adhibition_end_dt) {
         $suppliers = new MSupplier();
         $suppliers = $suppliers->where('mst_suppliers_cd', '=', $supplier_cd)
