cfc850faa2d71cfe8cdb77f56d9ce482b0d5af4c
toanht <toanht@shinway.net>
2019-03-11 13:14:11 +0700

    Dashboard

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,8 @@
 @extends('Layouts.app')
-@section('title','')
+@section('title','メニュー')
 @section("style")
 @endsection
 @section('content')
-@php $screen='Dashboard'; @endphp
 @stop
 @section("scripts")
 @endsection
