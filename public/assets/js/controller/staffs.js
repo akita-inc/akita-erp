0d2b40f179a9a82f59d4301d58b55cbda656235d
sonpt@shinway.net <0938529947>
2019-03-26 17:29:16 +0700

    Merge branch 'develop' of http://git.shinway.net/akita/akita-erp into develop
    # Conflicts:
    #	public/assets/js/controller/customers.js
    #	public/assets/js/controller/staffs-list.js
    #	public/assets/js/controller/staffs.js

--- public/assets/js/controller/staffs.js
+++ public/assets/js/controller/staffs.js
@@ -25519,7 +25519,8 @@ module.exports = function(module) {
           currentValue: this.range ? [null, null] : null,
           userInput: null,
           popupVisible: !1,
-          position: {}
+          position: {},
+          id: Date.now()
         };
       },
       watch: {
@@ -25590,6 +25591,9 @@ module.exports = function(module) {
         },
         innerPopupStyle: function innerPopupStyle() {
           return D({}, this.position, this.popupStyle);
+        },
+        createIDByTime: function createIDByTime() {
+          return "datepicker" + this.id;
         }
       },
       mounted: function mounted() {
@@ -25725,6 +25729,25 @@ module.exports = function(module) {
           this.$emit("blur", e);
         },
         handleFocus: function handleFocus(e) {
+          var t = !0,
+              n = !1,
+              a = void 0;
+
+          try {
+            for (var i, r = document.querySelectorAll(".mx-datepicker-popup")[Symbol.iterator](); !(t = (i = r.next()).done); t = !0) {
+              var s = i.value;
+              s.id.toString() !== "datepicker" + this.id.toString() ? s.style.display = "none" : s.style.display = "block";
+            }
+          } catch (e) {
+            n = !0, a = e;
+          } finally {
+            try {
+              !t && r.return && r.return();
+            } finally {
+              if (n) throw a;
+            }
+          }
+
           this.popupVisible || (this.popupVisible = !0), this.$emit("focus", e);
         },
         handleKeydown: function handleKeydown(e) {
@@ -25871,6 +25894,9 @@ module.exports = function(module) {
         ref: "calendar",
         staticClass: "mx-datepicker-popup",
         style: e.innerPopupStyle,
+        attrs: {
+          id: e.createIDByTime
+        },
         on: {
           click: function click(e) {
             e.stopPropagation(), e.preventDefault();
@@ -26749,7 +26775,7 @@ var CACHE = [],
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! D:\petproject\akita-erp\resources\assets\js\controller\staffs-vl.js */"./resources/assets/js/controller/staffs-vl.js");
+module.exports = __webpack_require__(/*! E:\MyProject\akita-erp\resources\assets\js\controller\staffs-vl.js */"./resources/assets/js/controller/staffs-vl.js");
 
 
 /***/ })
