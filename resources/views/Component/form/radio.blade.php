091d9ccb2ff641400d38f15834b4a16d40681d49
phuongcm <phuongcm@shinway.net>
2019-04-02 16:43:38 +0700

    Feature #23025 空車登録（新規）

--- resources/views/Component/form/radio.blade.php
+++ resources/views/Component/form/radio.blade.php
@@ -2,9 +2,11 @@
     <label class="{!! isset($required) && $required ? "required":"" !!}" for="{!! $filed !!}">
         {{ trans(@$prefix.$filed) }}
     </label>
+    <div class="pl-3">
     @foreach($array as $key=>$value)
         <input type="radio" id="{!! $filed.$key !!}" name="{!! $filed !!}" v-model="field.{!! $filed !!}" class="form-control form-check-input" value="{!!  $key!!}">
         <span class="form-check-label" for="{!! $filed.$key !!}">{!! $value !!}</span>
     @endforeach
+    </div>
 </div>
 
