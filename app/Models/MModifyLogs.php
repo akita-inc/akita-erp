51013e8399103f94796132e6f85d9df8019f515f
phuongcm <phuongcm@shinway.net>
2019-04-09 09:30:45 +0700

    #23131, #23133 該当データが削除済みの状態で更新ボタン押下時の処理追加（車両-修正）

---
+++ app/Models/MModifyLogs.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ptson
+ * Date: 04/09/2019
+ * Time: 9:29 AM
+ */
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+
+class MModifyLogs extends Model
+{
+    const CREATED_AT = 'created_at';
+}
