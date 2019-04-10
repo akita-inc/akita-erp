aa38ad2798d62f18a3ed6af755d3ea3727a4360a
sonpt@shinway.net <0938529947>
2019-04-10 15:43:05 +0700

    #23148

--- app/Http/Middleware/UpdateLogRouters.php
+++ app/Http/Middleware/UpdateLogRouters.php
@@ -29,9 +29,29 @@ class UpdateLogRouters extends Middleware
             $acceplog->url = $link;
             $acceplog->mst_staff_id = Auth::user()->id;
             $acceplog->http_user_agent = $_SERVER["HTTP_USER_AGENT"];
-            $acceplog->ip_address = $_SERVER["REMOTE_ADDR"];
+            //$acceplog->ip_address = $_SERVER["REMOTE_ADDR"];
+            $acceplog->ip_address = $this->get_client_ip_server();
             $acceplog->save();
         }
         return $next($request);
+    }
+    protected  function get_client_ip_server() {
+        $ipaddress = '';
+        if ($_SERVER['HTTP_CLIENT_IP'])
+            $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+        else if($_SERVER['HTTP_X_FORWARDED_FOR'])
+            $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        else if($_SERVER['HTTP_X_FORWARDED'])
+            $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+        else if($_SERVER['HTTP_FORWARDED_FOR'])
+            $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+        else if($_SERVER['HTTP_FORWARDED'])
+            $ipaddress = $_SERVER['HTTP_FORWARDED'];
+        else if($_SERVER['REMOTE_ADDR'])
+            $ipaddress = $_SERVER['REMOTE_ADDR'];
+        else
+            $ipaddress = 'UNKNOWN';
+
+        return $ipaddress;
     }
 }
