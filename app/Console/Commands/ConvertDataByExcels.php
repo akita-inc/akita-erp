90753f785fc04d4880cd287a986f30d9757820ac
sonpt@shinway.net <0938529947>
2019-04-17 15:51:04 +0700

    #import data

--- app/Console/Commands/ConvertDataByExcels.php
+++ app/Console/Commands/ConvertDataByExcels.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Console\ImportExcel\MstStaffDependents;
 use App\Models\MBusinessOffices;
 use App\Models\MCustomers;
 use App\Models\MEmptyInfo;
@@ -53,155 +54,9 @@ class ConvertDataByExcels extends Command
      */
     public function handle(){
         $type = $this->option("type");
-        switch ($type){
-            case 'mst_staffs':
-                $path = config('params.import_file_path.mst_staffs');
-                break;
-            case 'mst_staff_dependents':
-                $path = "";
-                break;
-            case 'mst_staff_qualifications':
-                $path = "";
-                break;
-            case 'mst_vehicles':
-                $path = config('params.import_file_path.mst_vehicles.main');
-                break;
-            case 'mst_customers':
-                $path = "";
-                break;
-            case 'mst_suppliers':
-                $path = "";
-                break;
-            case 'mst_business_offices':
-                $path = "";
-                break;
-        }
-        $this->getDataFromExcel($path, $type);
-    }
-
-    protected function getDataFromExcel($path, $type){
-        try {
-            $inputFileType = \PHPExcel_IOFactory::identify($path);
-            $objReader = \PHPExcel_IOFactory::createReader($inputFileType);
-            $objPHPExcel = $objReader->load($path);
-        } catch(Exception $e) {
-            return ('Error loading file "'.pathinfo($path,PATHINFO_BASENAME).'": '.$e->getMessage());
-        }
-        $sheet = $objPHPExcel->getSheet(0);
-        $highestRow = $sheet->getHighestRow();
-        $highestColumn = $sheet->getHighestColumn();
-        $keys = array();
-        $start_row = 2;
-        for ($row = $start_row; $row <= $highestRow; $row++){
-            $rowData = $sheet->rangeToArray('A' . $row . ':' . $highestColumn . $row,null,false,false, true);
-            switch ($type){
-                case 'mst_staffs':
-                    $this->importStaffs($rowData,$row);
-                    break;
-                case 'mst_staff_dependents':
-                    break;
-                case 'mst_staff_qualifications':
-                    break;
-                case 'mst_vehicles':
-                    $this->importVehicle($rowData, $row);
-                    break;
-                case 'mst_customers':
-                    break;
-                case 'mst_suppliers':
-                    break;
-                case 'mst_business_offices':
-                    break;
-            }
-        }
-    }
-
-    public function importStaffs($rowData,$row){
-        $model= new MStaffs();
-        $excel_column = $model->excel_column;
-        $record = array();
-        $mGeneralPurposes = new MGeneralPurposes();
-        foreach($rowData[$row] as $pos=>$value){
-            if(isset($excel_column[$pos])) {
-                switch ($excel_column[$pos]){
-                    case 'created_at':
-                    case 'modified_at':
-                        $record[$excel_column[$pos]] = \PHPExcel_Style_NumberFormat::toFormattedString($value,'mm/dd/yyyy hh:mm:ss');
-                        break;
-                    default:
-                        $record[$excel_column[$pos]] = $value;
-                }
-            }
-
-        }
-        WriteLogs::initLog();
-        dd($record);
-    }
-    public function importVehicle($rowData, $row){
-        $model =  new MVehicles();
-        $excel_column = $model->excel_column;
-        $record = array();
-        $data = [];
-        $mGeneralPurposes = new MGeneralPurposes();
-        foreach($rowData[$row] as $pos=>$value){
-            if(isset($excel_column[$pos])) {
-                switch ($excel_column[$pos]){
-                    case 'created_at':
-                    case 'modified_at':
-                        $record[$excel_column[$pos]] = \PHPExcel_Style_NumberFormat::toFormattedString($value,'mm/dd/yyyy hh:mm:ss');
-                        break;
-                    case 'vehicles_kb':
-                    case 'vehicle_size_kb':
-                    case 'vehicle_purpose_id':
-                    case 'land_transport_office_cd':
-                    case 'vehicle_classification_id':
-                    case 'private_commercial_id':
-                    case 'car_body_shape_id':
-                    case 'vehicle_id':
-                    case 'kinds_of_fuel_id':
-                        switch ($excel_column[$pos]){
-                            case 'vehicles_kb':
-                                $data_kb = config('params.data_kb')['vehicles_kb'];
-                                break;
-                            case 'vehicle_size_kb':
-                                $data_kb = config('params.data_kb')['vehicle_size_kb'];
-                                break;
-                            case 'vehicle_purpose_id':
-                                $data_kb = config('params.data_kb')['vehicle_purpose'];
-                                break;
-                            case 'land_transport_office_cd':
-                                $data_kb = config('params.data_kb')['land_transport_office_cd'];
-                                break;
-                            case 'vehicle_classification_id':
-                                $data_kb = config('params.data_kb')['vehicle_classification'];
-                                break;
-                            case 'private_commercial_id':
-                                $data_kb = config('params.data_kb')['private_commercial'];
-                                break;
-                            case 'car_body_shape_id':
-                                $data_kb = config('params.data_kb')['car_body_shape'];
-                                break;
-                            case 'vehicle_id':
-                                $data_kb = config('params.data_kb')['vehicle'];
-                                break;
-                            case 'kinds_of_fuel_id':
-                                $data_kb = config('params.data_kb')['kinds_of_fuel'];
-                                break;
-                        }
-                        $result = $mGeneralPurposes->checkExistDataAndInsert($data_kb,$value);
-                        if($result){
-                            $record[$excel_column[$pos]] = $result;
-                        }else{
-                            break 2;
-                        }
-                        break;
-                    default:
-                        $record[$excel_column[$pos]] = $value;
-                }
-            }
-          if(!empty($record)){
-            DB::table('mst_vehicles_copy1')->insert($record);
-          }
-        }
-
+        $class = "\\APP\\Console\\Commands\\ImportExcel\\".str_replace(" ","",(ucwords(str_replace("_"," ",$type))));
+        $class = new $class();
+        $class->type = $type;
+        $class->run();
     }
 }
