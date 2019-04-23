a0c18656025aad6a360a7f1ee9b259fa5dbbeeac
toanht <toanht@shinway.net>
2019-04-23 12:55:35 +0700

    Feature #23154 【データコンバート】社員

--- app/Console/Commands/ImportExcel/MstStaffs.php
+++ app/Console/Commands/ImportExcel/MstStaffs.php
@@ -39,11 +39,11 @@ class MstStaffs extends BaseImport
         'AA'=>'notes',
         'AB' => 'created_at',
         'AE' => 'modified_at',
-//        'S'=>'staff_dependents_nm_1',
-//        'T'=>'staff_dependents_nm_2',
-//        'U'=>'staff_dependents_nm_3',
-//        'V'=>'staff_dependents_nm_4',
-//        'W'=>'staff_dependents_nm_5'
+        'S'=>'staff_dependents_nm_S',
+        'T'=>'staff_dependents_nm_T',
+        'U'=>'staff_dependents_nm_U',
+        'V'=>'staff_dependents_nm_V',
+        'W'=>'staff_dependents_nm_W'
     ];
     public $column_main_name=[
         'staff_cd'=>'社員CD',
@@ -63,6 +63,7 @@ class MstStaffs extends BaseImport
         'created_at' => '登録日',
         'modified_at' => '最終更新日',
         'insurer_number'=>'保険番号',
+        'landline_phone_number'=>'電話番号',
         'basic_pension_number'=>'基礎年金番号',
         'person_insured_number'=>'被保険者番号',
         'health_insurance_class'=>'健康保険等級',
@@ -84,8 +85,6 @@ class MstStaffs extends BaseImport
         'drivers_license_number'=>'免許証番号',
         'drivers_license_issued_dt'=>'書換年月日'
     ];
-    public $labels=[];
-    public $messagesCustom=[];
     public $ruleValid = [
         'staff_cd'  => 'required|length:5|unique:mst_staffs,staff_cd',
         'last_nm'  => 'nullable|length:25',
@@ -282,6 +281,7 @@ class MstStaffs extends BaseImport
     public function mainReading($rowData,$row){
         $excel_column = $this->excel_column;
         $record = array();
+        $recordStaffDepents=array();
         $mGeneralPurposes = new MGeneralPurposes();
         $insuranceArr=$this->getDataFromChildFile(config('params.import_file_path.mst_staffs.health_insurance_card_information'),'insurance');
         $backgroundArr=$this->getDataFromChildFile(config('params.import_file_path.mst_staffs.staff_background'),'staff_background');
@@ -386,6 +386,14 @@ class MstStaffs extends BaseImport
                             break;
                     }
                 }
+                if(isset($excel_column[$pos])  && strpos($excel_column[$pos], 'staff_dependents_nm') !== false)
+                {
+                    if(!empty($value))
+                    {
+                        $recordStaffDepents[]=$record['staff_dependents_nm_'.$pos];
+                    }
+                    unset($record['staff_dependents_nm_'.$pos]);
+                }
                 $record["password"]=bcrypt($this->generateRandomString(8));
                 $record['belong_company_id']=$this->getBelongCompanyId();
                 $record['enable_fg']=true;
@@ -406,7 +414,7 @@ class MstStaffs extends BaseImport
             }
             if(!empty($record))
             {
-                $this->validateRow($record);
+                $record=$this->validateRow($record);
             }
             if(!empty($record) && $this->error_fg==false)
             {
@@ -422,6 +430,7 @@ class MstStaffs extends BaseImport
                 unset($record['staff_nm']);
                 unset($record['staff_nm_kana']);
                 unset($record["phone_number"]);
+                $record['staff_dependents']=$recordStaffDepents;
                 $this->insertDB($record);
             }
             else
@@ -432,33 +441,9 @@ class MstStaffs extends BaseImport
 
 
     }
-    public function insertMstStaffDependents($mst_staff_id,$value)
-    {
-        DB::beginTransaction();
-        try{
-            if(!empty($value))
-            {
-//                $arrInsert=[
-//                    'mst_staff_id'=>$mst_staff_id,
-//                    ''=>
-//                ];
-                DB::commit();
-            }
-            else
-            {
-                return;
-            }
-        }
-        catch (\Exception $e)
-        {
-            DB::rollBack();
-            return;
-        }
-    }
-    protected function validateRow($record){
+    public function validateRow($record){
         $this->error_fg=false;
         if( !empty($this->ruleValid)){
-            $validator = Validator::make( $record, $this->ruleValid ,$this->messagesCustom ,$this->labels );
             if(is_null($record['mst_business_office_id']))
             {
                 $this->error_fg=true;
@@ -469,52 +454,24 @@ class MstStaffs extends BaseImport
                     "extraFileName" => 'mst_business_offices',
                 ]));
             }
-            if ($validator->fails()) {
-                    $this->error_fg=true;
-                    $failedRules = $validator->failed();
-                    if(isset($failedRules['last_nm_kana']['KanaCustom']) || isset($failedRules['first_nm_kana']['KanaCustom']) )
-                    {
-                        $this->log("DataConvert_Err_KANA",Lang::trans("log_import.check_kana",[
-                            "fileName" =>  config('params.import_file_path.mst_staffs.main_file_name'),
-                            "fieldName" => $this->column_main_name['staff_nm_kana'],
-                            "row" => $this->rowIndex,
-                        ]));
-                    }
-                    if (isset($failedRules['staff_cd']['Unique'])) {
-                        $this->log("DataConvert_Err_ID_Match",Lang::trans("log_import.unique_cd",[
-                            "fileName" =>  config('params.import_file_path.mst_staffs.main_file_name'),
-                            "fieldName" => $this->column_main_name['staff_cd'],
-                            "row" => $this->rowIndex,
-                        ]));
-                    }
-                    foreach ($failedRules as $field => $errors){
-                            foreach ($errors as $ruleName => $error){
-                                if($ruleName=='Length'){
-                                        $this->log("DataConvert_Trim",Lang::trans("log_import.check_length_and_trim",[
-                                            "fileName" => config('params.import_file_path.mst_staffs.main_file_name'),
-                                            "excelFieldName" => $this->column_main_name[$field],
-                                            "row" => $this->rowIndex,
-                                            "excelValue" => $record[$field],
-                                            "tableName" => $this->table,
-                                            "DBFieldName" => $field,
-                                            "DBvalue" => substr($record[$field],0,$error[0]),
-                                        ]));
-                                    $record[$field] = substr($record[$field],0,$error[0]);
-                                }
-                                elseif($ruleName=='Required')
-                                {
-                                    $this->log("DataConvert_Err_required",Lang::trans("log_import.required",[
-                                        "fileName" => config('params.import_file_path.mst_staffs.main_file_name'),
-                                        "fieldName" => $this->column_main_name[$field],
-                                        "row" => $this->rowIndex,
-                                    ]));
-                                }
-                            }
-                    }
-                }
+            if(isset($record["driver_license"]))
+            {
+                $record["driver_license"]=$this->validateChildFile($record['driver_license'],'drivers_license_nm');
+            }
+            else
+            {
+                $this->error_fg=true;
+                $this->log("DataConvert_Err_ID_Match",Lang::trans("log_import.no_record_in_extra_file",[
+                    "mainFileName" => config('params.import_file_path.mst_staffs.main_file_name'),
+                    "fieldName" => $this->column_main_name['staff_cd'],
+                    "row" => $this->rowIndex,
+                    "extraFileName" => config('params.import_file_path.mst_staffs.drivers_license_nm'),
+                ]));
+            }
+
             if(isset($record['insurance']))
             {
-                $this->validateChildFile($record['insurance'],'health_insurance_card_information_nm');
+                $record['insurance']=$this->validateChildFile($record['insurance'],'health_insurance_card_information_nm');
             }
             else
             {
@@ -529,7 +486,7 @@ class MstStaffs extends BaseImport
 
             if(isset($record['staff_background']))
             {
-                $this->validateChildFile($record['staff_background'],'staff_background_nm');
+                $record['staff_background']=$this->validateChildFile($record['staff_background'],'staff_background_nm');
             }
             else
             {
@@ -541,32 +498,65 @@ class MstStaffs extends BaseImport
                     "extraFileName" => config('params.import_file_path.mst_staffs.staff_background_nm'),
                 ]));
             }
-            if(isset($record["driver_license"]))
-            {
-                $this->validateChildFile($record['driver_license'],'drivers_license_nm');
-            }
-            else
-            {
-                $this->error_fg=true;
-                $this->log("DataConvert_Err_ID_Match",Lang::trans("log_import.no_record_in_extra_file",[
-                    "mainFileName" => config('params.import_file_path.mst_staffs.main_file_name'),
-                    "fieldName" => $this->column_main_name['staff_cd'],
-                    "row" => $this->rowIndex,
-                    "extraFileName" => config('params.import_file_path.mst_staffs.drivers_license_nm'),
-                ]));
-            }
+            $validator = Validator::make( $record, $this->ruleValid );
+            if ($validator->fails()) {
+                    $failedRules = $validator->failed();
+                    foreach ($failedRules as $field => $errors){
+                        foreach ($errors as $ruleName => $error) {
+                            if ($ruleName == 'Length') {
+                                $this->log("DataConvert_Trim", Lang::trans("log_import.check_length_and_trim", [
+                                    "fileName" => config('params.import_file_path.mst_staffs.main_file_name'),
+                                    "excelFieldName" => $this->column_main_name[$field],
+                                    "row" => $this->rowIndex,
+                                    "excelValue" => $record[$field],
+                                    "tableName" => $this->table,
+                                    "DBFieldName" => $field,
+                                    "DBvalue" => substr($record[$field], 0, $error[0]),
+                                ]));
+                                $record[$field] = substr($record[$field], 0, $error[0]);
+                            };
+                            if($ruleName == 'Required')
+                            {
+                                $this->error_fg = true;
+                                    $this->log("DataConvert_Err_required", Lang::trans("log_import.required", [
+                                        "fileName" => config('params.import_file_path.mst_staffs.main_file_name'),
+                                        "fieldName" => $this->column_main_name[$field],
+                                        "row" => $this->rowIndex,
+                                    ]));
+                            };
+
+                        }
+                    }
+                    if(isset($failedRules['last_nm_kana']['KanaCustom']) || isset($failedRules['first_nm_kana']['KanaCustom']) )
+                    {
+                        $this->error_fg=true;
+                        $this->log("DataConvert_Err_KANA",Lang::trans("log_import.check_kana",[
+                            "fileName" =>  config('params.import_file_path.mst_staffs.main_file_name'),
+                            "fieldName" => $this->column_main_name['staff_nm_kana'],
+                            "row" => $this->rowIndex,
+                        ]));
+                    }
+                    if (isset($failedRules['staff_cd']['Unique'])) {
+                        $this->error_fg=true;
+                        $this->log("DataConvert_Err_ID_Match",Lang::trans("log_import.unique_cd",[
+                            "fileName" =>  config('params.import_file_path.mst_staffs.main_file_name'),
+                            "fieldName" => $this->column_main_name['staff_cd'],
+                            "row" => $this->rowIndex,
+                        ]));
+                    }
+                    }
 
         }
+        return $record;
     }
-    protected function validateChildFile($recordChildFile,$filename)
+    public function validateChildFile($recordChildFile,$filename)
     {
-        $validatorChild = Validator::make( $recordChildFile, $this->ruleValid ,$this->messagesCustom ,$this->labels );
+        $validatorChild = Validator::make( $recordChildFile, $this->ruleValid);
         if ($validatorChild->fails()) {
             $failedRules = $validatorChild->failed();
             foreach ($failedRules as $field => $errors) {
                 foreach ($errors as $ruleName => $error) {
                     if ($ruleName == 'Length') {
-                        $this->error_fg=true;
                         $this->log("DataConvert_Trim", Lang::trans("log_import.check_length_and_trim", [
                             "fileName" => config('params.import_file_path.mst_staffs.'.$filename),
                             "excelFieldName" => $this->column_main_name[$field],
@@ -576,19 +566,28 @@ class MstStaffs extends BaseImport
                             "DBFieldName" => $field,
                             "DBvalue" => substr($recordChildFile[$field], 0, $error[0]),
                         ]));
+                        $recordChildFile[$field] = substr($recordChildFile[$field],0,$error[0]);
                     }
                 }
             }
         }
+        return $recordChildFile;
     }
     public function insertDB($record)
     {
         DB::beginTransaction();
         try{
-            if(DB::table('mst_staffs')->insert($record))
+            $recordStaffDependents=$record['staff_dependents'];
+            unset($record['staff_dependents']);
+            $id = DB::table($this->table)->insertGetId( $record );
+            if($id)
              {
                  $this->numNormal++;
                  DB::commit();
+                 if(count($recordStaffDependents)>0)
+                 {
+                     $this->insertMstStaffDependents($id,$recordStaffDependents);
+                 }
              };
         }catch (\Exception $e){
             $this->numErr++;
@@ -598,6 +597,38 @@ class MstStaffs extends BaseImport
                 "row" => $this->rowIndex,
                 "errorDetail" => $e->getMessage(),
             ]));
+        }
+    }
+    public function insertMstStaffDependents($mst_staff_id,$recordStaffDependents)
+    {
+
+        DB::beginTransaction();
+        try{
+            $staffDependents=$recordStaffDependents;
+            $arrInsert=array();
+             foreach ($staffDependents as $key=>$value )
+             {
+                 $staff_nm=$this->explodeStaffName($value,null);
+                 $arrInsert[]=[
+                        'mst_staff_id'=>$mst_staff_id,
+                        'last_nm'=>empty($staff_nm['last_nm'])?null:$staff_nm['last_nm'],
+                        'first_nm'=>empty($staff_nm["first_nm"])?null:$staff_nm["first_nm"],
+                    ];
+             }
+             if(DB::table('mst_staff_dependents')->insert($arrInsert))
+             {
+                    DB::commit();
+             }
+        }
+        catch (\Exception $e)
+        {
+            $this->log("DataConvert_Err_SQL",Lang::trans("log_import.insert_error",[
+                "fileName" => config('params.import_file_path.mst_staff_dependents'),
+                "row" => $this->rowIndex,
+                "errorDetail" => $e->getMessage(),
+            ]));
+            DB::rollBack();
+            return;
         }
     }
     protected function exportPassword()
