82fe2940fb07708a75c3b7b4861d09d031d3d33d
sonpt@shinway.net <0938529947>
2019-04-24 11:23:12 +0700

    #23248

---
+++ app/Console/Commands/RunBatchImport.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sonpt
+ * Date: 4/24/2019
+ * Time: 11:13 AM
+ */
+namespace App\Console\Commands;
+
+
+
+class RunBatchImport extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'RunBatchImport';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '
+コンバート用のバッチを一括で処理するバッチを作成してください。
+【処理概要】
+1.テーブルのTrancate
+2.汎用マスタの登録
+3.営業所マスタ作成バッチ実行
+4.社員マスタ作成バッチ実行
+5.社員扶養者マスタ作成バッチ実行
+6.社員扶養者マスタ作成バッチ実行
+7.社員保有資格マスタ作成バッチ実行
+8.車両マスタ作成バッチ実行
+9.得意先マスタ作成バッチ実行
+10.仕入先マスタ作成バッチ実行
+11.請求書発行先住所マスタ作成バッチ実行
+';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle(){
+        $type = $this->option("type");
+        $class = "\\APP\\Console\\Commands\\ImportExcel\\".str_replace(" ","",(ucwords(str_replace("_"," ",$type))));
+        $class = new $class();
+        $class->table = $type;
+        $class->run();
+    }
+}
