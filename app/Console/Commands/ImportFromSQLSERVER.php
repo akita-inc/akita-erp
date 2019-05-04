f0a34a72a52eb7ef7cc012dfb3114c235d1f42cc
sonpt@shinway.net <0938529947>
2019-05-04 14:41:39 +0700

    #23248

--- app/Console/Commands/ImportFromSQLSERVER.php
+++ app/Console/Commands/ImportFromSQLSERVER.php
@@ -43,28 +43,33 @@ class ImportFromSQLSERVER extends Command
      * @return mixed
      */
     public function handle(){
+        /**
+         * Created by PhpStorm.
+         * User: sonpt
+         * Date: 5/4/2019
+         * Time: 2:31 PM
+         */
         $serverName = "172.30.30.193";
         $connectionInfo = [
             "Database" => "AKITA",
             "Uid" => "sa",
-            "PWD" => "Shinway@123"
+            "PWD" => "Shinway@123",
+            "CharacterSet" => "UTF-8"
         ];
         $conn = sqlsrv_connect( $serverName, $connectionInfo);
         if( $conn )
         {
-            $sql = "SELECT TOP 10 * FROM ".mb_convert_encoding('M_運転日報', 'sjis-win', 'UTF-8');
+            $sql = "SELECT TOP 10 * FROM M_運転日報";
             $stmt = sqlsrv_query( $conn, $sql );
             if( $stmt === false) {
                 die( print_r( sqlsrv_errors(), true) );
             }
 
             while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC) ) {
-                echo $row['block_name'];
+                echo $row['伝票NO'];
             }
-
-            sqlsrv_free_stmt( $stmt);
-
             echo "Connection established.\n";
+
         }
         else
         {
@@ -72,9 +77,9 @@ class ImportFromSQLSERVER extends Command
             die( print_r( sqlsrv_errors(), true));
         }
 
-        //-----------------------------------------------
-        // Perform operations with connection.
-        //-----------------------------------------------
+//-----------------------------------------------
+// Perform operations with connection.
+//-----------------------------------------------
 
         /* Close the connection. */
         sqlsrv_close( $conn);
