5785ce1c12298427b72d5ec10a07b00bd0ddeb44
toanht <toanht@shinway.net>
2019-05-15 15:12:51 +0700

    Feature #23414 【ワークフロープロジェクト】ワークフロー一覧

---
+++ resources/assets/js/service/type-service.js
@@ -0,0 +1,37 @@
+type_list_service = {
+    loadList: function (data) {
+        return axios.post('/type/api-v1/getItems', data).then(function (response) {
+            return response.data;
+        }).catch(function (error) {
+            return error;
+        });
+    },
+    checkIsExist: function (id,data) {
+        return axios.post('/type/api-v1/checkIsExist/' + id, data).then(function (response) {
+            return response.data;
+        }).catch(function (error) {
+            return error;
+        });
+    },
+    backHistory: function () {
+        return axios.get('/type/api-v1/back-history').then(function (response) {
+            return response.data;
+        }).catch(function (error) {
+            return error;
+        });
+    },
+    loadCustomerList: function (type) {
+        return axios.post('/type/api-v1/mst-customer-list',{type:type}).then(function (response) {
+            return response.data;
+        }).catch(function (error) {
+            return error;
+        });
+    },
+    createCSV: function (data) {
+        return axios.post('/type/api-v1/create-csv',data, {responseType: 'arraybuffer'}).then(function (response) {
+            return response;
+        }).catch(function (error) {
+            return error;
+        });
+    },
+}
