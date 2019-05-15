74e50c33c13283bae1d3a6d1b69945ef4d710dfb
thaontl <thaontl@shinway.net>
2019-05-15 13:29:52 +0700

    fix bug migration

--- database/migrations/2019_03_06_023306_edit_mst_staffs_table.php
+++ database/migrations/2019_03_06_023306_edit_mst_staffs_table.php
@@ -20,7 +20,7 @@ class EditMstStaffsTable extends Migration
             $table->date('adhibition_end_dt')->default('2999-12-31');
             $table->string('staff_cd',5);
             $table->string('password',50);
-            $table->string('remember_token',255);
+            $table->string('remember_token',255)->nullable();
             $table->integer('employment_pattern_id')->nullable();
             $table->string('position_id',5)->nullable();
             $table->string('last_nm',25)->nullable();
