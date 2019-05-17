77abd7e207dd6e0233d70c9713174086d61650d8
thaontl <thaontl@shinway.net>
2019-05-17 16:11:42 +0700

    edit table name

--- app/Console/Commands/DeleteJICONAXFromSQLSERVER.php
+++ app/Console/Commands/DeleteJICONAXFromSQLSERVER.php
@@ -68,7 +68,7 @@ class DeleteJICONAXFromSQLSERVER extends Command
         if( $this->connect )
         {
             $arrayDocumentNo = [];
-            $sql = "SELECT [伝票NO] FROM M_運転日報_copy WHERE CONVERT(datetime, [最終更新日]) > DATEADD(DAY, -".config("params.runDeleteLogicFromSqlServer.day_minus").",getdate())";
+            $sql = "SELECT [伝票NO] FROM M_運転日報 WHERE CONVERT(datetime, [最終更新日]) > DATEADD(DAY, -".config("params.runDeleteLogicFromSqlServer.day_minus").",getdate())";
             $stmt = sqlsrv_query( $this->connect, $sql );
             if( $stmt === false) {
                 die( print_r( sqlsrv_errors(), true) );
@@ -77,8 +77,8 @@ class DeleteJICONAXFromSQLSERVER extends Command
                 $arrayDocumentNo[$row["伝票NO"]] = $row["伝票NO"];
             }
             $this->deleteJiconax($arrayDocumentNo);
-            $this->deleteSalesORPurchases($arrayDocumentNo,"t_saleses_copy");
-            $this->deleteSalesORPurchases($arrayDocumentNo,"t_purchases_copy");
+            $this->deleteSalesORPurchases($arrayDocumentNo,"t_saleses");
+            $this->deleteSalesORPurchases($arrayDocumentNo,"t_purchases");
         }
         else
         {
@@ -96,7 +96,7 @@ class DeleteJICONAXFromSQLSERVER extends Command
 
     protected function deleteJiconax($arrayDocumentNo){
         $queryWhere = 'DATE_FORMAT(last_updated,"%Y/%m/%d") > DATE_FORMAT(DATE_SUB(NOW(),INTERVAL '.config("params.runDeleteLogicFromSqlServer.day_minus").' DAY),"%Y/%m/%d")';
-        $listJconaxModel = DB::table("t_jiconax_sales_datas_copy")->select("document_no")
+        $listJconaxModel = DB::table("t_jiconax_sales_datas")->select("document_no")
             ->whereNull("deleted_at")
             ->whereRaw($queryWhere)
             ->get();
@@ -104,7 +104,7 @@ class DeleteJICONAXFromSQLSERVER extends Command
             if( !isset($arrayDocumentNo[$value->document_no]) && !isset($this->arrayDocumentNoFails[$value->document_no]) ){
                 $this->arrayDocumentNoRead[$value->document_no] = $value->document_no;
                 try{
-                    DB::table("t_jiconax_sales_datas_copy")
+                    DB::table("t_jiconax_sales_datas")
                         ->where("document_no",$value->document_no)
                         ->update(["deleted_at"=>date("YmdHis")]);
                     $this->arrayDocumentNoSuccess[$value->document_no] = $value->document_no;
@@ -144,15 +144,15 @@ class DeleteJICONAXFromSQLSERVER extends Command
         unset($this->arrayDocumentNoSuccess[$document_no]);
         $this->arrayDocumentNoFails[$document_no] = $document_no;
         try {
-            DB::table("t_jiconax_sales_datas_copy")
+            DB::table("t_jiconax_sales_datas")
                 ->where("document_no",$document_no)
                 ->whereNotNull("deleted_at")
                 ->update(["deleted_at"=>DB::raw("Null")]);
-            DB::table("t_saleses_copy")
+            DB::table("t_saleses")
                 ->where("document_no",$document_no)
                 ->whereNotNull("deleted_at")
                 ->update(["deleted_at"=>DB::raw("Null")]);
-            DB::table("t_purchases_copy")
+            DB::table("t_purchases")
                 ->where("document_no",$document_no)
                 ->whereNotNull("deleted_at")
                 ->update(["deleted_at"=>DB::raw("Null")]);
