ea08b2e0e4f6c0af966d79c3276a94051b938644
Toan <toanhuynh345@gmail.com>
2019-05-31 10:41:12 +0700

    Feature #23412 ワークフロー-DB定義 マイグレーションファイル作成＆実行

--- database/migrations/2019_05_31_100958_create_wf_business_entertaining__expenses_table.php
+++ database/migrations/2019_05_31_100958_create_wf_business_entertaining__expenses_table.php
@@ -28,9 +28,9 @@ class CreateWfBusinessEntertainingExpensesTable extends Migration
             $table->tinyInteger('own_members_count')->nullable();
             $table->string('place',200)->nullable();
             $table->string('report',400)->nullable();
-            $table->decimal('deposit_amount',8,0)->nullable();
+            $table->decimal('deposit_amount',8,0)->nullable()->default(0);
             $table->integer('payoff_kb')->nullable();
-            $table->decimal('payoff_amount',8,0)->nullable();
+            $table->decimal('payoff_amount',8,0)->nullable()->default(0);
             $table->timestamp('delete_at')->nullable();
             $table->timestamp('create_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('modified_at')->default(DB::raw('CURRENT_TIMESTAMP'));
