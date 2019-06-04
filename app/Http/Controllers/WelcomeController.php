c17c78f5b51ee0f962337ca1ca5bdab9e2550021
phuongcm <phuongcm@shinway.net>
2019-06-04 09:59:11 +0700

    23580 【AKITA受入れ】課題対応

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\MBusinessOffices;
+use Illuminate\Support\Facades\Auth;
+
 class WelcomeController extends Controller
 {
     /**
@@ -11,6 +14,10 @@ class WelcomeController extends Controller
      */
     public function index()
     {
-        return view('welcome');
+        $mBusinessOffices = new MBusinessOffices();
+        $businessOfficeNm = $mBusinessOffices->select('id','business_office_nm')->where('id','=',Auth::user()->mst_business_office_id)->first();
+        return view('welcome',[
+            'businessOfficeNm' => $businessOfficeNm ? $businessOfficeNm->business_office_nm: null,
+        ]);
     }
 }
