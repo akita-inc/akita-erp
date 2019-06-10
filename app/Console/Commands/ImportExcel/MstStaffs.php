f5e1e63413c4a73248fb27ff8e41822d14d363d6
Toan <toanhuynh345@gmail.com>
2019-06-10 16:48:27 +0700

    【ワークフロープロジェクト】交際費申請（編集）

--- app/Console/Commands/ImportExcel/MstStaffs.php
+++ app/Console/Commands/ImportExcel/MstStaffs.php
@@ -21,6 +21,7 @@ use Illuminate\Support\Facades\Validator;
 class MstStaffs extends BaseImport
 {
     public $path = "";
+    public $table="mst_staffs_copy1";
     public $error_fg=false;
     public $belongCompanyId=null;
     public $prefCdByPrefNameCustom=[];
@@ -142,6 +143,7 @@ class MstStaffs extends BaseImport
     {
         $this->path = config('params.import_file_path.mst_staffs.main');
         date_default_timezone_set("Asia/Tokyo");
+        $this->table="mst_staffs_copy1";
         $this->dateTimeRun = date("YmdHis");
         $mGeneralPurposes=new MGeneralPurposes();
         $mBusinessOffices=new MBusinessOffices();
